8940f2be1dfad20ee23f48d8b604d829344b53db
Justin Tervay <justin@tervay.com>
2025-05-05 20:33:21 -0400

    Add sprockets

--- app/components/recalc/nav.tsx
+++ app/components/recalc/nav.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 export default function Nav() {
   return (
-    <header className="h-8 w-full bg-blue-600">
+    <header className="h-8 w-full bg-primary">
       <div className="flex h-full items-center justify-center">
         <Link to="/" className="flex items-center justify-center">
           <Mountain className="h-8 w-8 text-white" />
