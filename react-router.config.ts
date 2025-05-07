dc60323d829de1ca0cd70e23dee0677ebc7014c2
Justin Tervay <justin@tervay.com>
2025-05-07 02:37:37 -0400

    Add comlink

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from '@react-router/dev/config';
 import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
-  ssr: true,
+  ssr: false,
   presets: [vercelPreset()],
 } satisfies Config;
